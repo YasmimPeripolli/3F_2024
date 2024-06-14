4060ad60bbdaaaacaec3238499e54cc6180b02bb
Yasmim Peripolli <104101284+YasmimPeripolli@users.noreply.github.com>
2024-06-14 08:53:28 -0300

    Add files via upload
    14/06/2024

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const botoes= document.querySelectorAll(".botao");
 const conteudo= document.querySelectorAll(".aba-conteudo");
-const contador= document.querySelectorAll("contador");
+const contador= document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date(2024,11,17,10,50,0);
+const tempoObjetivo2= new Date(2024,11,20,11,50,0);
+const tempoObjetivo3= new Date(2024,10,20,11,00,0);
+const tempoObjetivo4= new Date(2024,9,17,10,50,0);
 
 for(let i = 0 ; i <botoes.length ; i++){
     botoes[i].onclick = function (){
@@ -10,6 +14,7 @@ conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
+ contador[0].textContent="Olá"
     }
 
 }
