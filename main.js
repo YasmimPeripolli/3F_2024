6714f9307ddf54b19244fe8ae2d1d2b523d8d9e3
Yasmim Peripolli <104101284+YasmimPeripolli@users.noreply.github.com>
2024-06-20 09:40:43 -0300

    Add files via upload
    20/06/2024

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const botoes= document.querySelectorAll(".botao");
 const conteudo= document.querySelectorAll(".aba-conteudo");
 const contador= document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date(2024,11,17,10,50,0);
-const tempoObjetivo2= new Date(2024,11,20,11,50,0);
-const tempoObjetivo3= new Date(2024,10,20,11,00,0);
-const tempoObjetivo4= new Date(2024,9,17,10,50,0);
+const tempoObjetivo1= new Date(2024,11,25,00,00,0);
+const tempoObjetivo2= new Date(2024,11,25,00,00,0);
+const tempoObjetivo3= new Date(2024,11,25,00,00,0);
+const tempoObjetivo4= new Date(2024,11,25,00,00,0);
+let  agora=new Date();
+contador[0].textContent=tempoObjetivo1-agora
+contador[1].textContent=tempoObjetivo2-agora
+contador[2].textContent=tempoObjetivo3-agora
+contador[3].textContent=tempoObjetivo4-agora
 
 for(let i = 0 ; i <botoes.length ; i++){
     botoes[i].onclick = function (){
@@ -14,7 +19,7 @@ conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
- contador[0].textContent="Olá"
+ 
     }
 
 }
