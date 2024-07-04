7c44eb0026670b8a30c28e0461dff99c14994de5
Yasmim Peripolli <104101284+YasmimPeripolli@users.noreply.github.com>
2024-07-04 09:43:58 -0300

    04/07

--- main.js
+++ main.js
@@ -24,10 +24,20 @@ segundos= (tempoObjetivo-agora)/1000;
  segundos = segundos%60;
  minutos= minutos%60;
  horas = horas%24;
-
+return [dias,horas,minutos,segundos]
 
 }
+let contadorPrimeiro = calculaTempo(tempoObjetivo1)
+contador[0].textContent = `Faltam ${contadorPrimeiro[0]} dias, ${contadorPrimeiro[1]} horas, ${contadorPrimeiro[2]} minutos, ${contadorPrimeiro[3]} segundos`
 
+let contadorSegundo = calculaTempo(tempoObjetivo2)
+contador[1].textContent = `Faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas, ${contadorSegundo[2]} minutos, ${contadorSegundo[3]} segundos`
+
+let contadorTerceiro = calculaTempo(tempoObjetivo3)
+contador[2].textContent = `Faltam ${contadorTerceiro[0]} dias, ${contadorTerceiro[1]} horas, ${contadorTerceiro[2]} minutos, ${contadorTerceiro[3]} segundos`
+
+let contadorQuarto = calculaTempo(tempoObjetivo4)
+contador[3].textContent = `Faltam ${contadorQuarto[0]} dias, ${contadorQuarto[1]} horas, ${contadorQuarto[2]} minutos, ${contadorQuarto[3]} segundos`
 
 contador[0].textContent=`Faltam ${dias} dias, ${horas} horas, ${minutos}, minutos e ${segundos} segundos`
 contador[1].textContent=tempoObjetivo2-agora
